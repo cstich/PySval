dbee340aa7f65f4ac2e520e1734950a8e837247b
Christoph Stich <christoph.n.stich@gmail.com>
2021-07-15 20:21:07 +0200

    Fixed version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name='sval',  
-    version='0.1.1',
+    version='0.1.2',
     description='A fast Hold\'em seven card evaluator ',  
     long_description=long_description,  
     long_description_content_type='text/markdown',
